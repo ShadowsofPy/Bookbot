bd43afa2ee3fa0aefabf15baa099fd6256afc137
Shadowsofpy <abenjamin1624@gmail.com>
2024-12-02 22:45:22 +0000

    added word count and updated main() function

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import string
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+    word_count(file_contents)
+
+def word_count(text):
+    words = text.split()
+    print(len(words))
 
 main()
