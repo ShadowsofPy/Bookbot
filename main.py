dcf278c9ad6685632fd937685ad7dbfbbd70cb8e
Shadowsofpy <abenjamin1624@gmail.com>
2024-12-03 15:25:35 +0000

    added character count

--- main.py
+++ main.py
@@ -3,10 +3,22 @@ import string
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-    word_count(file_contents)
+    words = word_count(file_contents)
+    print(f"Frankenstein has {words} words.")
+    characters = char_count(file_contents)
+    print(f"List of characters: {characters}")
 
 def word_count(text):
     words = text.split()
-    print(len(words))
+    return len(words)
 
+def char_count(text):
+    letters = {}
+    for char in text.lower():
+        if char in letters:
+            letters[char] += 1
+        else:
+            letters[char] = 1
+    return letters
+    
 main()
